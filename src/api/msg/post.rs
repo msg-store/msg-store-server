7460d88b0f31b4f0cdca2c0b9f354ecbf54db5ae
joshua enokson <kilograhm@pm.me>
2022-02-17 23:46:34 -0800

    updated msg post error handling

--- src/api/msg/post.rs
+++ src/api/msg/post.rs
@@ -3,9 +3,9 @@ use actix_web::{HttpResponse};
 use bytes::Bytes;
 use crate::AppData;
 use msg_store::api::error_codes;
-use msg_store::api::msg::add::{handle, Chunky};
+use msg_store::api::msg::add::{handle, Chunky, AddErrorTy, MsgError};
 use futures::{Stream, StreamExt};
-use log::info;
+use log::{error, info};
 use serde::{Deserialize, Serialize};
 use std::process::exit;
 use std::task::Poll;
@@ -40,43 +40,46 @@ pub struct ReturnBody {
     uuid: String,
 }
 
-const STATUS_400: &'static [&'static str] = &[
-    error_codes::COULD_NOT_GET_CHUNK_FROM_PAYLOAD,
-    error_codes::COULD_NOT_PARSE_CHUNK,
-    error_codes::MISSING_HEADERS,
-    error_codes::MALFORMED_HEADERS,
-    error_codes::INVALID_PRIORITY,
-    error_codes::MISSING_PRIORITY,
-    error_codes::INVALID_BYTESIZE_OVERRIDE,
-    error_codes::MISSING_BYTESIZE_OVERRIDE
-];
-const STATUS_403: &'static [&'static str] = &[
-    error_codes::FILE_STORAGE_NOT_CONFIGURED
-];
-const STATUS_409: &'static [&'static str] = &[
-    error_codes::MSG_EXCEEDES_STORE_MAX,
-    error_codes::MSG_EXCEEDES_GROUP_MAX,
-    error_codes::MSG_LACKS_PRIORITY
-];
-
 const ROUTE: &'static str = "POST /api/msg";
 pub async fn http_handle(data: Data<AppData>, body: Payload) -> HttpResponse {
     info!("{}", ROUTE);
     match handle(&data.store, &data.file_storage, &data.stats, &data.db, &mut PayloadBridge(body)).await {
         Ok(uuid) => HttpResponse::Ok().json(ReturnBody { uuid: uuid.to_string() }),
-        Err(error_code) => {
-            if STATUS_400.contains(&error_code) {
-                info!("{} 400 {}", ROUTE, error_code);
-                HttpResponse::BadRequest().body(error_code)
-            } else if STATUS_403.contains(&error_code) {
-                info!("{} 403 {}", ROUTE, error_code);
-                HttpResponse::Forbidden().body(error_code)
-            } else if STATUS_409.contains(&error_code) {
-                info!("{} 409 {}", ROUTE, error_code);
-                HttpResponse::Conflict().body(error_code)
-            } else {
-                error_codes::log_err(error_code, file!(), line!(), "");
-                exit(1)
+        Err(error) => {
+            match error.err_ty {
+                AddErrorTy::MsgError(msg_error) => {
+                    match msg_error {
+                        MsgError::InvalidBytesizeOverride |
+                        MsgError::InvalidPriority |
+                        MsgError::MalformedHeaders |
+                        MsgError::MissingBytesizeOverride |
+                        MsgError::MissingHeaders |
+                        MsgError::MissingPriority => {
+                            info!("{} 400 {}", ROUTE, msg_error);
+                            HttpResponse::BadRequest().body(msg_error.to_string())
+                        },
+                        MsgError::CouldNotGetNextChunkFromPayload |
+                        MsgError::CouldNotParseChunk => {
+                            info!("{} 400 {}", ROUTE, msg_error);
+                            HttpResponse::BadRequest().body(msg_error.to_string())
+                        },
+                        MsgError::FileStorageNotConfigured => {
+                            info!("{} 403 {}", ROUTE, msg_error);
+                            HttpResponse::Forbidden().body(msg_error.to_string())
+                        },
+                        MsgError::MsgExceedesGroupMax |
+                        MsgError::MsgExceedesStoreMax |
+                        MsgError::MsgLacksPriority => {
+                            info!("{} 409 {}", ROUTE, msg_error);
+                            HttpResponse::BadRequest().body(msg_error.to_string())
+                        }
+                    }
+                },
+                AddErrorTy::CouldNotFindFileStorage |
+                _ => {
+                    error!("{}", error);
+                    exit(1)
+                }
             }
         }
     }
